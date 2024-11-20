f64618ed399b5d1729620738e705d328f3a4d0ba
Ian Stroemkjaer Jensen <82367076+IanStroemkjaerJensen@users.noreply.github.com>
2024-11-20 17:52:25 +0100

    Add files via upload
    Common layer uploaded, but without the appsettings file since it contains confidential information and company secrets

---
+++ Care.Web.Common/ValueObjects/Error.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Care.Web.Common.ValueObjects
+{
+    public class Error
+    {
+        public string Code { get; }
+        public string Message { get; }
+        public int StatusCode { get; }
+
+        public Error(string code, string message, int statusCode = 400)
+        {
+            Code = code;
+            Message = message;
+            StatusCode = statusCode;
+        }
+
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Code);
+        }
+    }
+}
